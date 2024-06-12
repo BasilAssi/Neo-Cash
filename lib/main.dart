f4dcf3acf270ed22310980bc7a95e0e7c5bf15d9
Basil Assi <104434508+BasilAssi@users.noreply.github.com>
2024-06-12 19:41:00 +0300

    added debugShowCheckedModeBanner: false,

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,7 @@ class _MyAppState extends State<MyApp> {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
+      debugShowCheckedModeBanner: false,
       locale: _locale,
       supportedLocales: const [
         Locale('ar'),
