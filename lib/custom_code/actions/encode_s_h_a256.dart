bca15a28c188b936b497d8832601b5052bce628d
Basil Assi <104434508+BasilAssi@users.noreply.github.com>
2024-07-25 13:20:25 +0300

    updated pubspec.yaml ,,   // some actions

--- lib/custom_code/actions/encode_s_h_a256.dart
+++ lib/custom_code/actions/encode_s_h_a256.dart
@@ -6,16 +6,18 @@ import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom actions
 import '/flutter_flow/custom_functions.dart'; // Imports custom functions
 import 'package:flutter/material.dart';
+
+import 'dart:convert';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
 Future<String?> encodeSHA256(String? str) async {
-  try {
-    List<int> bytes = utf8.encode(str);
-    Digest digest = sha256.convert(bytes);
-    return base64.encode(digest.bytes).trim();
-  } catch (e) {
-    print(e);
+  // try {
+  //   List<int> bytes = utf8.encode(str);
+  //   Digest digest = sha256.convert(bytes);
+  //   return base64.encode(digest.bytes).trim();
+  // } catch (e) {
+  //   print(e);
     return null;
-  }
+  // }
 }
