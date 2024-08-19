6fca84dea767de638500ad09a64fa4bf11e62355
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-19 12:47:10 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/registeration/registeration_02/registeration02_widget.dart
+++ lib/pages/registeration/registeration_02/registeration02_widget.dart
@@ -858,7 +858,7 @@ class _Registeration02WidgetState extends State<Registeration02Widget> {
                                         citiesDropDownLOOKUPsAPIsResponse
                                             .jsonBody)!
                                     .records
-                                    .map((e) => e.encodedId)
+                                    .map((e) => e.code)
                                     .toList()
                                 : FFAppConstants.emptyListStrings),
                             optionLabels: FFLocalizations.of(context)
