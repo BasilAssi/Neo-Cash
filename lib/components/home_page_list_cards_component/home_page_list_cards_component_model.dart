459040c45b5b261e8c111ea5e3c4ac24fb6fdb1c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-28 06:59:15 +0000

    Updating to latest FlutterFlow output.

--- lib/components/home_page_list_cards_component/home_page_list_cards_component_model.dart
+++ lib/components/home_page_list_cards_component/home_page_list_cards_component_model.dart
@@ -1,4 +1,6 @@
+import '/backend/api_requests/api_calls.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import 'dart:async';
 import 'home_page_list_cards_component_widget.dart'
     show HomePageListCardsComponentWidget;
 import 'package:flutter/material.dart';
@@ -8,6 +10,7 @@ class HomePageListCardsComponentModel
     extends FlutterFlowModel<HomePageListCardsComponentWidget> {
   ///  State fields for stateful widgets in this component.
 
+  Completer<ApiCallResponse>? apiRequestCompleter;
   // State field(s) for SwipeableStack widget.
   late CardSwiperController swipeableStackController;
 
@@ -18,4 +21,20 @@ class HomePageListCardsComponentModel
 
   @override
   void dispose() {}
+
+  /// Additional helper methods.
+  Future waitForApiRequestCompleted({
+    double minWait = 0,
+    double maxWait = double.infinity,
+  }) async {
+    final stopwatch = Stopwatch()..start();
+    while (true) {
+      await Future.delayed(const Duration(milliseconds: 50));
+      final timeElapsed = stopwatch.elapsedMilliseconds;
+      final requestComplete = apiRequestCompleter?.isCompleted ?? false;
+      if (timeElapsed > maxWait || (requestComplete && timeElapsed > minWait)) {
+        break;
+      }
+    }
+  }
 }
