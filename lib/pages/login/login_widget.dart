c38564d2e8eff9fb238a82815b9e6ffd95976f24
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-29 15:31:57 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/login/login_widget.dart
+++ lib/pages/login/login_widget.dart
@@ -510,7 +510,7 @@ class _LoginWidgetState extends State<LoginWidget>
                                               borderRadius: 12.0,
                                               margin: const EdgeInsetsDirectional
                                                   .fromSTEB(
-                                                      16.0, 4.0, 16.0, 4.0),
+                                                      16.0, 0.0, 16.0, 0.0),
                                               hidesUnderline: true,
                                               isOverButton: true,
                                               isSearchable: false,
