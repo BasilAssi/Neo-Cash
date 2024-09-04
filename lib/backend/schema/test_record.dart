23fc87a36c7b0985cff5ebbc9937ae60fadcc80d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-04 14:26:38 +0000

    Updating to latest FlutterFlow output.

--- lib/backend/schema/test_record.dart
+++ lib/backend/schema/test_record.dart
@@ -20,56 +20,8 @@ class TestRecord extends FirestoreRecord {
   String get name => _name ?? '';
   bool hasName() => _name != null;
 
-  // "description" field.
-  String? _description;
-  String get description => _description ?? '';
-  bool hasDescription() => _description != null;
-
-  // "specifications" field.
-  String? _specifications;
-  String get specifications => _specifications ?? '';
-  bool hasSpecifications() => _specifications != null;
-
-  // "price" field.
-  double? _price;
-  double get price => _price ?? 0.0;
-  bool hasPrice() => _price != null;
-
-  // "created_at" field.
-  DateTime? _createdAt;
-  DateTime? get createdAt => _createdAt;
-  bool hasCreatedAt() => _createdAt != null;
-
-  // "modified_at" field.
-  DateTime? _modifiedAt;
-  DateTime? get modifiedAt => _modifiedAt;
-  bool hasModifiedAt() => _modifiedAt != null;
-
-  // "on_sale" field.
-  bool? _onSale;
-  bool get onSale => _onSale ?? false;
-  bool hasOnSale() => _onSale != null;
-
-  // "sale_price" field.
-  double? _salePrice;
-  double get salePrice => _salePrice ?? 0.0;
-  bool hasSalePrice() => _salePrice != null;
-
-  // "quantity" field.
-  int? _quantity;
-  int get quantity => _quantity ?? 0;
-  bool hasQuantity() => _quantity != null;
-
   void _initializeFields() {
     _name = snapshotData['name'] as String?;
-    _description = snapshotData['description'] as String?;
-    _specifications = snapshotData['specifications'] as String?;
-    _price = castToType<double>(snapshotData['price']);
-    _createdAt = snapshotData['created_at'] as DateTime?;
-    _modifiedAt = snapshotData['modified_at'] as DateTime?;
-    _onSale = snapshotData['on_sale'] as bool?;
-    _salePrice = castToType<double>(snapshotData['sale_price']);
-    _quantity = castToType<int>(snapshotData['quantity']);
   }
 
   static CollectionReference get collection =>
@@ -107,26 +59,10 @@ class TestRecord extends FirestoreRecord {
 
 Map<String, dynamic> createTestRecordData({
   String? name,
-  String? description,
-  String? specifications,
-  double? price,
-  DateTime? createdAt,
-  DateTime? modifiedAt,
-  bool? onSale,
-  double? salePrice,
-  int? quantity,
 }) {
   final firestoreData = mapToFirestore(
     <String, dynamic>{
       'name': name,
-      'description': description,
-      'specifications': specifications,
-      'price': price,
-      'created_at': createdAt,
-      'modified_at': modifiedAt,
-      'on_sale': onSale,
-      'sale_price': salePrice,
-      'quantity': quantity,
     }.withoutNulls,
   );
 
@@ -138,29 +74,11 @@ class TestRecordDocumentEquality implements Equality<TestRecord> {
 
   @override
   bool equals(TestRecord? e1, TestRecord? e2) {
-    return e1?.name == e2?.name &&
-        e1?.description == e2?.description &&
-        e1?.specifications == e2?.specifications &&
-        e1?.price == e2?.price &&
-        e1?.createdAt == e2?.createdAt &&
-        e1?.modifiedAt == e2?.modifiedAt &&
-        e1?.onSale == e2?.onSale &&
-        e1?.salePrice == e2?.salePrice &&
-        e1?.quantity == e2?.quantity;
+    return e1?.name == e2?.name;
   }
 
   @override
-  int hash(TestRecord? e) => const ListEquality().hash([
-        e?.name,
-        e?.description,
-        e?.specifications,
-        e?.price,
-        e?.createdAt,
-        e?.modifiedAt,
-        e?.onSale,
-        e?.salePrice,
-        e?.quantity
-      ]);
+  int hash(TestRecord? e) => const ListEquality().hash([e?.name]);
 
   @override
   bool isValidKey(Object? o) => o is TestRecord;
